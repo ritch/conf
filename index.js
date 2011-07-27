37e4adc4d4eca4b0bee707098ce0d64c4780a80b
Ritchie Martori <skawful@gmail.com>
2011-07-26 22:07:42 -0700

    tests

--- index.js
+++ index.js
@@ -2,4 +2,17 @@
 * conf
 */
 
-require('hive').init(__dirname);
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log("Listening on " + port);
+});
+
+//require('hive').init(__dirname);
