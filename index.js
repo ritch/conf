a8659a0a119962939e4d026128e7c0cc77d1dda4
Ritchie Martori <skawful@gmail.com>
2011-07-26 22:12:17 -0700

    changed config

--- index.js
+++ index.js
@@ -2,17 +2,4 @@
 * conf
 */
 
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-var port = process.env.PORT || 3000;
-app.listen(port, function(){
-  console.log("Listening on " + port);
-});
-
-//require('hive').init(__dirname);
+require('hive').init(__dirname);
