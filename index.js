754a2da4691ee4d1651ceca84426308f5a975152
Ritchie Martori <skawful@gmail.com>
2011-07-31 20:57:19 -0700

    more h

--- index.js
+++ index.js
@@ -3,5 +3,17 @@
 */
 
 console.log('starting in ', __dirname);
+require("express");
+require("backbone");
+require("mongoose");
+require("findit");
+require("rimraf");
+require("request");
+require("formidable");
+require("underscore");
+require("connect-form");
+require("hamljs");
+require("forever");
+require("ejs");
 
-require('hive').init(__dirname);
+//require('hive').init(__dirname);
