5928456c5ab594790583629fda3c5b686208ad2c
Ritchie Martori <skawful@gmail.com>
2011-07-31 21:00:21 -0700

    more h

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 */
 
 console.log('starting in ', __dirname);
-require("express");
+var e = require("express");
 require("backbone");
 require("mongoose");
 require("findit");
@@ -14,5 +14,13 @@ require("underscore");
 require("connect-form");
 require("hamljs");
 require("ejs");
+
+var app = e.createServer();
+
+app.get('/', function(req, res) {
+	res.send('...');
+});
+
+app.listen(process.env.PORT || 3000)
 
 //require('hive').init(__dirname);
