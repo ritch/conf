817d9245451612683e915c56ce4fc71499eb6e88
Ritchie Martori <skawful@gmail.com>
2011-08-02 00:45:54 -0700

    added skinning, and then some

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,37 @@
 var app;
 $(function() {
 
+	function now() {
+		var now = new Date();
+		return now.getTime() + (now.getTimezoneOffset() * 60000);
+	}
+	
+	function guid() {
+		return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+		    return v.toString(16);
+		});
+	}
+	
+	console.log(guid());
+
 	// ## Message Model
 	window.Message = Backbone.Model.extend({
 		
 		initialize: function() {
 			
 			var LIFE_PER_POINT = 5000,
-				born = Number(this.get('created') || new Date().getTime()),
+				born = Number(this.get('created') || now()),
 				death = born + 5000 + (this.get('points') * LIFE_PER_POINT);
 			
 			// TODO - this.get('created') seems to be off
-			console.log(born - new Date().getTime());
+			//console.log(born - new Date().getTime());
 			
 			this.set({death: death});
+			this.bind('change', function() {
+				this.collection.sort();
+				this.collection.trigger('change');
+			});
 			
 		},
 		
@@ -30,7 +48,8 @@ $(function() {
 		like: function() {
 			this.set({
 				points: this.get('points') + 1,
-				death: this.get('death') + 5000
+				death: this.get('death') + 5000,
+				updated: now()
 			});
 			if(!this.isNew()) this.save();
 		}
@@ -42,7 +61,7 @@ $(function() {
 		initialize: function() {
 			var _self = this;
 			setInterval(function() {
-				_self.flush(new Date().getTime());
+				//_self.flush(new Date().getTime());
 			}, 500);
 		},
 		
@@ -72,7 +91,8 @@ $(function() {
 		
 		//localStorage: new Store('Messages'),
 		comparator: function(msg) {
-			return msg.get('points');
+			//return msg.get('points');
+			return msg.get('updated') || msg.get('created');
 		},
 		
 		parse: function(response) {
@@ -165,21 +185,114 @@ $(function() {
 	
 	window.MessageView = Backbone.View.extend({
 		
+		initialize: function() {
+			var _self = this;
+		},
+		
 		tagName:  "li",
 		
 		template: _.template($('#message-template').html()),
 		
 		events: {
-			'click': 'like'
+			'click': 'like',
+			'mouseover': 'hover',
+			'mouseout': 'hover'
 		},
 		
 		render: function() {
 			this.model && $(this.el).html(this.template(this.model.toJSON()));
+			var status = new StatusView({el: this.$('canvas'), model: this.model});
+			if(this.model.hasChanged('updated')) {
+				this.flash();
+			}
 			return this;
 		},
 		
 		like: function() {
 			this.model.like();
+		},
+		
+		hover: function() {
+			console.log('this.hovered', this.hovered);
+			var color = this.hovered ? '#ffffff' : '#F4FFF5',
+				txt = this.hovered ? '#000000' : '#546657';
+			$(this.el).stop().animate({backgroundColor: color, color: txt}, 1000);
+			this.hovered = !this.hovered;
+		},
+		
+		flash: function() {
+			var start = '#F7F6E3',
+				stop = '#ffffff';
+				
+			$(this.el).stop().animate({backgroundColor: start}, 1000, function() {
+				$(this).animate({backgroundColor: stop}, 500)
+			});
+		}
+		
+	});
+	
+	window.StatusView = Backbone.View.extend({
+		initialize: function(attrs) {
+			this.render();
+		},
+		
+		render: function() {
+			var canvas = this.el[0]
+			  , context = canvas.getContext("2d")
+			  , percent = this.model.get('points') / 10;
+			
+			console.log(percent);
+			
+			context.moveTo(0, 0);
+			context.lineWidth = 10;
+			var radius = 22;
+			var x = 27;
+			var y = 27;
+			// The angle of each of the eight segments is 45 degrees (360 divided by 8), which
+			// equals p/4 radians.
+			var parts = 100;
+			var angleDelta = Math.PI / (parts / 2);
+			// Find the distance from the circle's center to the control points for the curves.
+			var ctrlDist = radius/Math.cos(angleDelta/2);
+			// Initialize the angle to 0 and define local variables that are used for the 
+			// control and ending points. 
+			var angle = 0;
+			var rx, ry, ax, ay;
+			// Move to the starting point, one radius to the right of the circle's center.
+			context.moveTo(x + radius, y);
+			var finishing = false;
+			
+			// Repeat eight times to create eight segments.
+			for (var i = 0; i < parts; i++) {
+				// Increment the angle by angleDelta (p/4) to create the whole circle (2p).
+				angle += angleDelta;
+				// The control points are derived using sine and cosine.
+				rx = x + Math.cos(angle-(angleDelta/2))*(ctrlDist);
+				ry = y + Math.sin(angle-(angleDelta/2))*(ctrlDist);
+				// The anchor points (end points of the curve) can be found similarly to the 
+				// control points.
+				ax = x + Math.cos(angle)*radius;
+				ay = y + Math.sin(angle)*radius;
+				// Draw the segment.
+				if(!!(i % 2)) {
+					if(i / parts > percent && !finishing) {
+						context.lineTo(rx, ry);
+						finishing = true;
+						context.strokeStyle = "#C6ED47";
+						context.stroke();
+						context.beginPath();
+					}
+					context.lineTo(rx, ry);
+				} else {	
+					context.moveTo(rx, ry);
+				}
+			}
+			context.lineTo(x + radius, y);
+			context.strokeStyle = "#F4F4F4";
+			if(percent >= 1) {
+				context.strokeStyle = '#C6ED47';
+			}
+			context.stroke();
 		}
 		
 	});
@@ -199,9 +312,7 @@ $(function() {
 			_.bindAll(_self, 'render');
 			_self.model.locate();
 			_self.model.messages().bind('all', function(e) {
-				var now = new Date()
-				  , utcNow = now.getTime() + (now.getTimezoneOffset() * 60000);
-				_self.model.set({updated: utcNow});
+				_self.model.set({updated: now()});
 				_self.render();
 			});
 			_self.model.bind('change:position', function() {
@@ -210,6 +321,7 @@ $(function() {
 		},
 		
 		render: function() {
+			$("html, body").animate({ scrollTop: $(document).height() + 200 }, 500);
 			var msgs = this.$('#messages').empty();
 			this.model.messages().each(function(msg) {
 				var mv = new MessageView({model: msg});
@@ -223,9 +335,9 @@ $(function() {
 				// enter was pressed
 				var el = $(e.target)
 				  , txt = el.val()
-				  , msg = txt && new Message({message: txt, position: this.model.get('position')});
+				  , msg = txt && new Message({message: txt, position: this.model.get('position'), created: now(), updated: now()});
 				if(msg) {
-					this.model.messages().add(msg);
+					//this.model.messages().add(msg);
 					msg.save();
 					el.val(null);
 				}
